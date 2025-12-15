4e1ff47ddd1e1047376af4fe02699c7b12781381
borg <daniel.s.birkeland@gmail.com>
2025-12-15 15:20:55 +0100

    Adding database error page for database get failures

--- controllers/con-default.js
+++ controllers/con-default.js
@@ -2,7 +2,11 @@
 const index = (req, res) => {
     res.render('index', { title: "Homepage" })
 }
+const missingdb = (req, res) => {
+    res.render('missingdb', { title: "Database error" })
+}
 
 module.exports = {
-    index
+    index,
+    missingdb
 }
--- routers/rou-user.js
+++ routers/rou-user.js
@@ -1,14 +1,25 @@
 const router = require('express').Router()
 const mid_auth = require('../middleware/mid-auth')
+const controller_def = require('../controllers/con-default')
 const controller = require('../controllers/con-user')
+const db = require('../handler/dbhandler')
 
-router.get('/login', controller.login_get);
+db.connectDB()
+.then((resu)=>{
+    if (resu)
+    {
+        router.get('/login', controller.login_get)
 
-router.post('/login', controller.login_post);
+        router.post('/login', controller.login_post)
 
-router.get('/profile', controller.profile_get);
+        router.get('/profile', controller.profile_get)
 
-router.post('/logout', controller.logout_post);
-
+        router.post('/logout', controller.logout_post)
+    }
+    else
+    {
+        router.use(controller_def.missingdb)
+    }
+})
 
 module.exports = router;

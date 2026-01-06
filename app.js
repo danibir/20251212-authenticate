21efaf5f1122a7a8cdc753851286894d9d841d7e
borg <daniel.s.birkeland@gmail.com>
2026-01-06 14:11:07 +0100

    Fixing db in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 3000
 const rou_default = require('./routers/rou-default')
 const rou_user = require('./routers/rou-user')
+const db = require('./handler/dbhandler')
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
@@ -11,10 +12,15 @@ app.use(express.json())
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(rou_default)
-app.use(rou_user)
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-});
+db.connectDB()
+.then((resu)=>{
+    
+        app.listen(3000)
+        app.use('/', rou_default)
+        if (resu)
+        {
+            console.log(`Server is running on http://localhost:${PORT}`)
+            app.use('/', rou_user)
+        }
+    })
